5b4191e96ef7d9d5979ce6b94b726d11ad542b8e
Aastha <awasthiaastha1999@gmail.com>
2022-03-15 22:58:21 +0530

    initial commit

---
+++ authentication/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def sign_up(request):
+    fm = UserCreationForm()
+    return render(request , 'authentication/signup.html',{'form':fm})
+    
